cbc05b06ca31826abed350e5525f9336f206856d
Kannan R S <77399238+kriskannan08@users.noreply.github.com>
2026-03-18 10:55:08 +0530

    Updated binary

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppSecurityKit",
-            url: "https://github.com/kriskannan08/appSecurityKit_iOS/releases/download/v1.1.0/AppSecurityKit.xcframework.zip",
-            checksum: "8c280dee705ad63ac633c13b94c47df440223180430ea6dc87269ccc086a1b0c"
+            url: "https://github.com/kriskannan08/appSecurityKit_iOS/releases/download/v1.1.1/AppSecurityKit.xcframework.zip",
+            checksum: "71b893c8b477e1ac92db5db79e1c1888b296b65f8f0c089002df1789b3fc047d"
         )
     ]
 )
